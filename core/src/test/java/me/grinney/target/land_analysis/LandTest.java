ac844447b3c08594ff81dde1d62068ef8c1d8255
Geoffrey J. Grinney <geoffrey@Geoffreys-MBP.PK5001Z>
2018-08-04 23:49:15 -0500

    Adding example test.

--- core/src/test/java/me/grinney/target/land_analysis/LandTest.java
+++ core/src/test/java/me/grinney/target/land_analysis/LandTest.java
@@ -44,20 +44,26 @@ public class LandTest {
 
     @Test
     void testComputeFertileAreas() {
-        Land land = new Land(400, 600);
-        land.addBarrenPlot(0, 292, 399, 307);
-        List<Integer> areas = land.computeFertileAreas();
-        assertEquals((int) areas.get(0), 116800);
-        assertEquals((int) areas.get(1), 116800);
-    }
-
-    @Test
-    void testComputeFertileAreas2() {
         Land land = new Land(4, 6);
         land.addBarrenPlot(0, 1, 3, 4);
         List<Integer> areas = land.computeFertileAreas();
         assertEquals((int) areas.get(0), 4);
         assertEquals((int) areas.get(1), 4);
+
+        land = new Land(400, 600);
+        land.addBarrenPlot(0, 292, 399, 307);
+        areas = land.computeFertileAreas();
+        assertEquals((int) areas.get(0), 116800);
+        assertEquals((int) areas.get(1), 116800);
+
+        land = new Land(400, 600);
+        land.addBarrenPlot(48, 192, 351, 207);
+        land.addBarrenPlot(48, 392, 351, 407);
+        land.addBarrenPlot(120, 52, 135, 547);
+        land.addBarrenPlot(260, 52, 275, 547);
+        areas = land.computeFertileAreas();
+        assertEquals((int) areas.get(0), 22816);
+        assertEquals((int) areas.get(1), 192608);
     }
 
 }
